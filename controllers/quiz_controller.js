a2d049cad59f1f7a28f4a12f4b02189f203603ed
Jose Alberto Mateos Roiz <jaalpaca@hotmail.com>
2015-07-23 17:47:59 +0200

    Ejercicio Obligatorio Modulo 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -15,12 +15,31 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(
-    function(quizes) {
-      res.render('quizes/index', { quizes: quizes});
-    }
-  ).catch(function(error) { next(error);})
-};
+  if(typeof(req.query.search) != 'undefined'){
+    var stringBusqueda='%'+req.query.search.replace(/[ ]/g,"%")+'%';
+    console.log(stringBusqueda);
+    models.Quiz.findAll({
+				where: [
+					"pregunta like ?", stringBusqueda]
+					, order: 'pregunta ASC'
+			}
+		).then(function(quizes) {
+				if(typeof(quizes != 'undefined')){
+					res.render('quizes/index', { quizes: quizes});
+				}
+			}
+		).catch(function(error) { next(error);}
+		)
+  }
+  else {
+    models.Quiz.findAll().then(
+        function(quizes) {
+          res.render('quizes/index', { quizes: quizes});
+        }
+      ).catch(function(error) { next(error);})
+    };
+  }
+
 
 // GET /quizes/:id
 exports.show = function(req, res) {
