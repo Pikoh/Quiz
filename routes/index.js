e3d535e857e7c395539707ac535918cbddf0fc85
Jose Alberto Mateos Roiz <jaalpaca@hotmail.com>
2015-07-24 10:05:21 +0200

    Creación de preguntas

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,8 @@ router.param('quizId', quizController.load);  // autoload :quizId
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
 router.get('/author',                      authorController.author);
+router.post('/quizes/create',              quizController.create);
 
 module.exports = router;
