f7eda7d193562e4e8401714eeafaa220ef453dce
Jose Alberto Mateos Roiz <jaalpaca@hotmail.com>
2015-07-24 11:51:58 +0200

    Edición de preguntas

--- routes/index.js
+++ routes/index.js
@@ -20,5 +20,7 @@ router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new',                  quizController.new);
 router.get('/author',                      authorController.author);
 router.post('/quizes/create',              quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        quizController.update);
 
 module.exports = router;
