77a032ca2eea77f8bb7dac8a49d28d6002a95f69
Jose Alberto Mateos Roiz <jaalpaca@hotmail.com>
2015-07-27 10:16:27 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/', function(req, res, next) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -33,5 +34,7 @@ router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired,quizC
 
 router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+	                                    sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
